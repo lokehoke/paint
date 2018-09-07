283667301b206bc1626c291f9db7f64d1d6742fc
lokeHoke <ibymif2000@yandex.ru>
2018-09-07 21:55:21 +0300

    enviroment

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const Webpack = require('webpack');
+
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+let config = {
+    watch: true,
+    devtool: 'source-map',
+    mode: 'development',
+    entry: {
+        main: [
+            './src/scss/index.scss',
+            './src/js/index.js'
+        ]
+    },
+    output: {
+		path: __dirname + '/public/resources/js/',
+		filename: 'bundle.js'
+	},
+    module: {
+        rules: [{
+            test: /\.scss$/,
+			use: [{
+				loader: 'style-loader'
+			}, {
+				loader: 'css-loader',
+				options: {
+					sourceMap: true
+				}
+			}, {
+				loader: 'postcss-loader',
+				options: {
+					plugins: [
+						autoprefixer({
+							browsers:['ie >= 8', 'last 4 version']
+						})
+					],
+					sourceMap: true
+				}
+			}, {
+				loader: 'sass-loader', options: {
+					sourceMap: true
+				}
+			}]
+        }, {
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						'@babel/preset-env',
+						'@babel/preset-react'
+					],
+					plugins: ['@babel/plugin-proposal-class-properties']
+				}
+			}
+		}]
+	}, plugins: [
+		new Webpack.NoEmitOnErrorsPlugin()
+	]
+};
+
+module.exports = config;
