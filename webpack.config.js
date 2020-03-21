2a22f8436d25a4f11ad41a29df531a4ce220eaae
lokehoke <ibymif2000@yandex.ru>
2020-03-21 21:27:43 +0300

    fix configs

--- webpack.config.js
+++ webpack.config.js
@@ -5,63 +5,86 @@ const Webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 
-let config = {
+module.exports = {
     watch: true,
     devtool: 'source-map',
     mode: 'development',
-    entry: {
-        main: [
+	entry:
+	{
+		main:
+		[
             '@babel/polyfill',
             './src/scss/index.scss',
-            './src/js/index.js'
-        ]
+            './src/js/index.js',
+        ],
     },
-    output: {
-		path: __dirname + '/public/resources/js/',
-		filename: 'bundle.js'
+	output:
+	{
+		path: path.join(__dirname, 'public/resources/js'),
+		filename: 'bundle.js',
 	},
-    module: {
-        rules: [{
-            test: /\.scss$/,
-			use: [{
-				loader: 'style-loader'
-			}, {
-				loader: 'css-loader',
-				options: {
-					sourceMap: true
-				}
-			}, {
-				loader: 'postcss-loader',
-				options: {
-					plugins: [
-						autoprefixer({
-							browsers:['ie >= 8', 'last 4 version']
-						})
-					],
-					sourceMap: true
-				}
-			}, {
-				loader: 'sass-loader', options: {
-					sourceMap: true
-				}
-			}]
-        }, {
-			test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: [
-						'@babel/preset-env',
-						'@babel/preset-react'
-					],
-					plugins: ['@babel/plugin-proposal-class-properties']
-				}
+	module:
+	{
+		rules:
+		[
+			{
+            	test: /\.scss$/,
+				use:
+				[
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader',
+						options:
+						{
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options:
+						{
+							plugins:
+							[
+								autoprefixer(),
+							],
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options:
+						{
+							sourceMap: true,
+						},
+					}
+				]
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use:
+				{
+					loader: 'babel-loader',
+					options:
+					{
+						presets:
+						[
+							'@babel/preset-env',
+							'@babel/preset-react',
+						],
+						plugins:
+						[
+							'@babel/plugin-proposal-class-properties',
+						],
+					},
+				},
 			}
-		}]
-	}, plugins: [
-		new Webpack.NoEmitOnErrorsPlugin()
-	]
+		]
+	},
+	plugins:
+	[
+		new Webpack.NoEmitOnErrorsPlugin(),
+	],
 };
-
-module.exports = config;
