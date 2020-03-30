3e11d8662fc86a9e68777cd4c29a00b10e122d33
lokehoke <ibymif2000@yandex.ru>
2020-03-30 23:33:05 +0300

    Update check_coverage.sh

--- scripts/check_coverage.sh
+++ scripts/check_coverage.sh
@@ -1,1 +1,3 @@
- yarn nyc --reporter=text mocha '**/src/**/test/**/*.ts' -r ts-node/register
+echo "\n #paint# Check coverage \n"
+
+yarn nyc --reporter=text mocha '**/src/**/test/**/*.ts' -r ts-node/register
